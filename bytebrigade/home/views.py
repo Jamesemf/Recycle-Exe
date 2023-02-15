44f6d58bedfd49c4571037aa5a96cfc5ab51a396
sccthomas <95318179+sccthomas@users.noreply.github.com>
2023-02-15 17:01:17 +0000

    Trying to fetch branch

--- bytebrigade/home/views.py
+++ bytebrigade/home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Transactions
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def getTransactions(request):
-    query_results = Transactions.objects.all()
-    return render(request, 'home/index.html', query_results)
+    data = Transactions.objects.all()
+    data_dict = {
+        'Transaction': data
+    }
+    return render(request, 'home/index.html', data_dict)
