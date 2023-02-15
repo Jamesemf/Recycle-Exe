3d04106048ebe64fe71e68f80d28ce9c370f6963
sccthomas <95318179+sccthomas@users.noreply.github.com>
2023-02-15 15:33:14 +0000

    Creating Databases and Index Page
    Co-Authored-By: AngeloPalmer <95857532+AngeloPalmer@users.noreply.github.com>
    Co-Authored-By: HowardCui-Super <78500597+HowardCui-Super@users.noreply.github.com>
    Co-Authored-By: James Ford <57077523+Jamesemf@users.noreply.github.com>
    Co-Authored-By: camh223 <95890146+camh223@users.noreply.github.com>
    Co-Authored-By: Oscar Norris <57996381+oscarhfnorris@users.noreply.github.com>

--- bytebrigade/home/models.py
+++ bytebrigade/home/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import time
 # Create your models here.
+class BinData(models.Model):
+    binId = models.CharField(max_length=10, primary_key=True)  # Format AM-01-1 (Building-Floor-BinNo)
+    binLat = models.DecimalField(max_digits=25, decimal_places=22)
+    binLong = models.DecimalField(max_digits=25, decimal_places=22)
+
+    GENERAL = 'General Waste'
+    RECYCLING = 'Recycling'
+    PLASTIC = 'Plastic'
+    PAPER = 'Paper'
+    CANS = 'Cans'
+    GLASS = 'Glass'
+
+    binTypeChoices = [
+        (GENERAL, 'General Waste'),
+        (RECYCLING, 'Recycling'),
+        (PLASTIC, 'Plastic'),
+        (PAPER, 'Paper'),
+        (CANS, 'Cans'),
+        (GLASS, 'Glass'),
+    ]
+
+    binType = models.CharField(
+        max_length=25,
+        choices=binTypeChoices,
+        default=GENERAL
+    )
+
+
+class Product(models.Model):
+    barcode = models.CharField(max_length=30, primary_key=True)
+    name = models.CharField(max_length=30)
+    details = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products')
+    type = models.CharField(max_length=30)
+    value = models.IntegerField()
+
+
+class Transactions(models.Model):
+    time = models.DateTimeField("Time occurred", default=time.time())
+    bin = models.ForeignKey(BinData, on_delete=models.CASCADE)  # need to assign as foreign key to bin application
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
