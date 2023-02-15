e7ec0d7a16c555e65b50a8530d4987138b10bdb7
sccthomas <95318179+sccthomas@users.noreply.github.com>
2023-02-15 16:00:54 +0000

    Home Page table
    Co-Authored-By: James Ford <57077523+Jamesemf@users.noreply.github.com>

--- bytebrigade/home/views.py
+++ bytebrigade/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Transactions
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return render(request, 'home/index.html')
+def getTransactions(request):
+    query_results = Transactions.objects.all()
+    return render(request, 'home/index.html', query_results)
