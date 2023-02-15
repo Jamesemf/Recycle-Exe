5e552e93d37fe1f39970ab67f5900389da20c1e7
HowardCui-Super <haoyangcui@outlook.com>
2023-02-15 15:22:29 +0000

    Product Model
    Create products model for database
    
    Co-Authored-By: camh223 <95890146+camh223@users.noreply.github.com>

--- bytebrigade/barcodereader/models.py
+++ bytebrigade/barcodereader/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    barcode = models.CharField(max_length=30, primary_key=True)
+    name = models.CharField(max_length=30)
+    details = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products')
+    type = models.CharField(max_length=30)
+    value = models.IntegerField()
