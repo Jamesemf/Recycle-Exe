5172d241b9361126da47d448eeb5bf7675dfc149
HowardCui-Super <haoyangcui@outlook.com>
2023-02-26 15:39:56 +0000

    Templates and Navbar Extension
    Functional bottoms on each templates
    
    @me extend all templates based on navbar.html
    
    @sccthomas modified and improved navbar
    
    Co-Authored-By: sccthomas <95318179+sccthomas@users.noreply.github.com>

--- bytebrigade/bytebrigade/settings.py
+++ bytebrigade/bytebrigade/settings.py
@@ -7,6 +7,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,7 +54,9 @@ ROOT_URLCONF = 'bytebrigade.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'statics/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
