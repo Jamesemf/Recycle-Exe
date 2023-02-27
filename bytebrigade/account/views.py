9bde3c1a33fba3abaab8002ba8c3829376913861
James <JamesEMford@outlook.com>
2023-02-27 18:00:59 +0000

    ANGELO and JAMES
    - Call data for profile and fix UI

--- bytebrigade/account/views.py
+++ bytebrigade/account/views.py
@@ -28,8 +28,15 @@ def register(request):
 def account(request):
     if request.user.is_authenticated:
         data = Statistic.objects.get(user=request.user)
+        maxWeek = Statistic.objects.all().order_by('curweek')[0]
+        maxMonth = Statistic.objects.all().order_by('curmonth')[0]
+        maxYear = Statistic.objects.all().order_by('curyear')[0]
+
         data_dict = {
             'Profile': data,
+            'maxWeek': maxWeek,
+            'maxMonth': maxMonth,
+            'maxYear': maxYear,
         }
         return render(request, 'account/Profile_page.html', data_dict)
     else:
