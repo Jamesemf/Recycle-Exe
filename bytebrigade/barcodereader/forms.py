33439567c868362b6150e3aa3d8d286afbf66329
sccthomas <95318179+sccthomas@users.noreply.github.com>
2023-02-27 20:03:28 +0000

    Barcode collected
    Barcode collected
    
    Co-Authored-By: Howard Cui <78500597+HowardCui-Super@users.noreply.github.com>
    Co-Authored-By: James Ford <57077523+Jamesemf@users.noreply.github.com>

--- bytebrigade/barcodereader/forms.py
+++ bytebrigade/barcodereader/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from home.models import Product
 
 
-class barcode_form(forms.ModelForm):
+class barcode_form(forms.Form):
     type = forms.CharField()
     barcode = forms.CharField()
 
