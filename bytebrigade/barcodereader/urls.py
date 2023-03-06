15e288744474962cc4b5d466883a7e1a9ae158c7
AngeloPalmer <angelo.s.palmer@gmail.com>
2023-03-06 19:43:27 +0000

    Creation of Bin Map
    Usage of Google Maps API to create map of campus' bins.
    
    Co-Authored-By: James Ford <57077523+Jamesemf@users.noreply.github.com>

--- bytebrigade/barcodereader/urls.py
+++ bytebrigade/barcodereader/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
         path('', views.barcode_lookup, name='barcode_lookup'),
         path('recycle/', views.recycle_confirm, name='recycle_confirm'),
         path('recycle/create/', views.create_product, name='create_product'),
+        path('bin_map', views.bin_map, name='bin_map')
     ]
