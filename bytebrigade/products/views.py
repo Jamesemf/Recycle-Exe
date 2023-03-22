d7777994dc7cb69215db81c8a5d7d75b7b97648a
sccthomas <95318179+sccthomas@users.noreply.github.com>
2023-03-22 15:44:20 +0000

    Fix2

--- bytebrigade/products/views.py
+++ bytebrigade/products/views.py
@@ -97,6 +97,7 @@ def prompt_recycle_product_view(request):
         binType = check_bin(product)
         if request.method == 'POST':
             shortestDistance, close_bin, bin_object = withinRange(request, binType)
+            print(bin_object.binName)
             request.session['newHome'] = bin_object.binId  # Directly correlates to a bin
             request.session['shownMap'] = 1
             return redirect("bin_map")
